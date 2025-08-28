a52fe45fdef36723b61d7616ceb8b317e2b7c663
Alexandr Vladimirovich Bazhenow <g_likhachev@mail.ru>
2025-08-28 06:22:24 +0300

    Add files via upload

---
+++ Игра Правитель/Unit2.pas
@@ -0,0 +1,26 @@
+unit Unit2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls;
+
+type
+  TForm2 = class(TForm)
+    RadioGroup1: TRadioGroup;
+    Button1: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
